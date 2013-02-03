39d0b20ab5f82b1428760c6b3e70f5beb13371c8
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-03 15:35:21 +0900

    modify: index assets

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <title>Jarallax</title>
 <meta content='Jarallax' name='description' />
 <meta content='en' http-equiv='content-language' />
-<link href='./css/main.css' rel='stylesheet' type='text/css' />
-<script src='./js/jquery-1.9.0.min.js' type='text/javascript'></script>
-<script src='./js/jarallax-0.2.3b.min.js' type='text/javascript'></script>
+<link href='./assets/css/app.css' rel='stylesheet' type='text/css' />
+<script src='./assets/js/jquery-1.9.0.min.js' type='text/javascript'></script>
+<script src='./assets/js/jarallax-0.2.3b.min.js' type='text/javascript'></script>
 </head>
 <body>
 <section class='container'>
